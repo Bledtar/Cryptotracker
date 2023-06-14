fac13bc92b136a049353204e42a50f18503a9c84
Nicola Culik <nicola.culik@eternalnet.ch>
2023-06-14 17:49:18 +0200

    erstes design

--- cryptotracker.py
+++ cryptotracker.py
@@ -7,21 +7,30 @@ from tkinter import ttk
 from DatabaseMgmt.databaseMgmt import DatabaseMgmt
 from Logic.calc import Calc 
 
-class Budgetplan(ctk.CTk):
+class Cointracker(ctk.CTk):
     DM = DatabaseMgmt()
     CB = Calc()
+
+    def back(self):
+        if len(self.frames) > 1:
+            self.frames[-1].pack_forget()  # Hide the current frame
+            self.frames.pop()  # Remove the current frame from the list
+        if self.frames:
+            self.frames[-1].place()
+
+
 
     def __init__(self):
         super().__init__()
 
         self.title('Cryptotracker')
-        self.geometry('1200x600')
+        self.geometry('1300x600')
 
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
         self.frames = []
         self.createMainFrame()
-        
+
     def createMainFrame(self):
         column = ['Coin', 'Betrag', 'Menge', 'Durch. Kaufpreis', 'Jetziger Preis', 'Gewinn/Verlust']
         self.entryCoinIDMapping = {}
@@ -41,7 +50,7 @@ class Budgetplan(ctk.CTk):
         btnAddTransaction.place(relx=0.5, y=100, anchor='center')
         btnAddTransaction.configure(width=100, height=30)
 
-        btnAddCoin = ctk.CTkButton(master=leftFrame, text="Neuer Coin")
+        btnAddCoin = ctk.CTkButton(master=leftFrame, text="Neuer Coin", command=self.CoinWindow)
         btnAddCoin.place(relx=0.5, y=150, anchor='center')
         btnAddCoin.configure(width=100, height=30)
 
@@ -70,7 +79,40 @@ class Budgetplan(ctk.CTk):
         label = ctk.CTkLabel(master=frameTransaction, text="Neue Transaktion", font=("Roboto" ,24))
         label.place(x=20)
 
+    def CoinWindow(self):
+        for frame in self.frames:
+            frame.destroy()
+        
+        column = ['Coin']
+
+        frameCoin = ctk.CTkFrame(self)
+        frameCoin.pack(fill='both', anchor='center', padx=100)
+        self.frames.append(frameCoin)
+
+        label = ctk.CTkLabel(master=frameCoin, text="Neuer Coin", font=("Roboto" ,24))
+        label.grid(pady=10)
+
+        self.entryCategory = ctk.CTkEntry(master=frameCoin, placeholder_text="Coin")
+        self.entryCategory.grid(pady=10)
+
+        self.btnSaveCoin = ctk.CTkButton(master=frameCoin, text="Speichern")
+        self.btnSaveCoin.grid(pady=10)
+
+        self.tableCoins = ttk.Treeview(master=frameCoin)
+        self.tableCoins["columns"] = column
+
+        for col in column:
+            self.tableCoins.column(col, width=150)
+            self.tableCoins.heading(col, text=col.capitalize())
+        self.tableCoins["show"] = "headings"
+        self.tableCoins.grid(pady=10)
+
+        self.btnDeleteCoin = ctk.CTkButton(master=frameCoin, text="Löschen")
+        self.btnDeleteCoin.grid(pady=10)
+
+        self.btnBack = ctk.CTkButton(master=frameCoin, text="Zurück", command=self.back)
+        self.btnBack.grid(pady=10)
 
 # run app
-app = Budgetplan()
+app = Cointracker()
 app.mainloop()
